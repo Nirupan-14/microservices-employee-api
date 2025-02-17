e9ceb17d41942a2c5b7d398503e0f0056ce6a4b0
Nirupan-14 <160653519+Nirupan-14@users.noreply.github.com>
2025-02-18 00:05:30 +0530

    Add files via upload

---
+++ microservices-employee-api/src/main/java/com/example/Employee/Controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.example.Employee.Controller;
+
+import com.example.Employee.Model.Employee;
+import com.example.Employee.Service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService service;
+
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return service.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return service.getAllEmployees();
+    }
+}
---
+++ microservices-employee-api/src/main/java/com/example/Employee/Model/Employee.java
@@ -0,0 +1,17 @@
+package com.example.Employee.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private Long departmentId; // Foreign key to Department
+}
